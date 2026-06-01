3cb5712279a650e0d376306617d23a22e9e969d2
Binaya764 <bigyanlimbu179@gmail.com>
2026-06-01 18:11:02 +0545

    routed added

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,8 +2,8 @@ const {Router} = require("express");
 const userRouter = Router();
 const userController = require("../controllers/userController");
 
-userRouter.get("/", userController.loginPage);
-userRouter.get("/create",userController.signUpPage);
-userRouter.post("/create",userController.createUser);
+userRouter.get("/", userController.loginPage);          //page once the user goes to the website
+userRouter.get("/create",userController.signUpPage);    //gets the signup page
+userRouter.post("/create",userController.createUser);   //post the user data from signup page to backend
 
 module.exports = userRouter;
