4b629cb3bca6c8fd317c1916ba1ffc441b177303
Binay Bista <binay.bista.009@gmail.com>
2026-06-13 08:36:37 +0545

    Creted functions so that input data form user can be transferd to backend

--- client/src/Components/Login-Signup/Login.jsx
+++ client/src/Components/Login-Signup/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import "./LoginSignup.css"
 
 import email_icon from '../../assets/email.png'
@@ -10,27 +11,65 @@ const Login = () => {
 
   const navigate = useNavigate();
 
-//  const [action,setAction] = useState("Sign Up");
-//   // creating usestate variable
-//   // useState is a react tool which provide us with a [var,func] func can be used to set data of this variable
+  //creating variables to capture login details form user
+  const [ email , setEmail ] = useState(""); //setEmail is a function that can change value of email
+  const [password , setPassword] = useState("");
+  const [errorMessage , setErrorMessage] = useState("");
+
+  //function to run when login button is pressed
+  const handleLogin = async() => {
+    setErrorMessage(""); //clears past errors
+
+    try{
+      //now we should make an api request to running backend port.
+      const response = await axios.post('http://localhost:5000/api/auth/login',{email: email, password: password}); //awati: wait till server response
+      //after obtaining response , response is stored in response.
+
+      if(response.data.success){
+        alert("Welcome back!");
+        // navigate("/dashboard");
+      }
+
+    }//to handle errors if server is down or details dosent match
+      catch (error){
+        if(error.response){
+          setErrorMessage(error.response.data.message);
+        }
+        else{
+          setErrorMessage("Cannot Connect to server. Backend might not be running");
+        }
+      }
+      
+
+  };
+
 
   return (
-    <div className="page-layout-rapper">
+    <div className="page-layout-wrapper">
       <div className='container'>
         <div className='header'>
           <div className='text'>Login</div>
           <div className='underline'></div>
         </div>
 
+        {errorMessage && <div style={{ color: 'red', textAlign: 'center', marginTop: '10px' }}>{errorMessage}</div>}
+
         <div className='inputs'>
           <div className='input'>
             <img src={email_icon} alt="" />
-            <input type="email" placeholder='Email Id'/>
+            <input type="email" 
+              placeholder='Email Id' 
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}/> {/*On change works every time an new character is entered or altered, e means event*/}
           </div>
 
           <div className='input'>
             <img src={password_icon} alt="" />
-            <input type="password" placeholder='Password'/>
+            <input type="password" 
+              placeholder='Password' 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              onKeyDown={(e) => {if (e.key === 'Enter') {handleLogin();}}}/>
           </div>
         </div>
 
@@ -38,7 +77,7 @@ const Login = () => {
         
         <div className="submit-container">
           <div className="submit gray" onClick={()=>navigate('/Signup')} >Sign Up</div>
-          <div className="submit">Login</div>
+          <div className="submit" onClick={handleLogin}>Login</div>
         </div>
       </div>
     </div>
